8bf106f1c47a0d195e1cb2c56a629e714b662693
Prashanth Kumar G <gprashanthkreddy@gmail.com>
2026-07-25 11:21:00 +0530

    Point frontend at deployed backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import auditRoutes from './routes/auditRoutes.js';
 import { auditRateLimiter } from './middleware/rateLimiter.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
+
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
